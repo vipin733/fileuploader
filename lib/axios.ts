2e1d3574cd042113bc2f0d3624ca6e90e4adf07e
phani <vipinoo7@yahoo.in>
2022-08-04 13:30:46 +0530

    fixed axios interceptor

--- lib/axios.ts
+++ lib/axios.ts
@@ -5,7 +5,7 @@ axios.interceptors.response.use( (response) =>  {
     return response;
 },  (error: AxiosError) =>  {    
     if (error.response?.status === 401) {
-        return Router.push("/")
+        return Router.push("/login")
     }
     return Promise.reject(error);
 });
